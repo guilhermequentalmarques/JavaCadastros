76198ad2c2c77c3eb9001b7783efda1d478a3d24
guilhermequentalmarques <guilherme.quental.marques@gmail.com>
2023-04-28 16:49:26 -0300

    quarto commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -6,35 +7,41 @@ public class Main {
     public static  void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Pessoa> colaboradores = new ArrayList<>();
-        System.out.println("Quantas pessoas deseja cadastrar");
-        int nmr = sc.nextInt();
-        int i;
+        try {
 
-        for(i = 0; i < nmr; i++){
-            System.out.print("Digite o nome do colaborador: ");
-            String nome = sc.nextLine();
 
-            System.out.print("Digite o CPF do colaborador: ");
-            String cpf = sc.nextLine();
+            System.out.println("Quantas pessoas deseja cadastrar");
+            int nmr = sc.nextInt();
+            int i;
 
-            System.out.print("Digite o telefone do colaborador: ");
-            String telefone = sc.nextLine();
+            for (i = 0; i < nmr; i++) {
+                System.out.print("Digite o nome do colaborador: ");
+                String nome = sc.nextLine();
 
-            System.out.print("Digite a data de nascimento do colaborador: ");
-            String dataNascimento = sc.nextLine();
+                System.out.print("Digite o CPF do colaborador: ");
+                String cpf = sc.nextLine();
 
-            System.out.print("Digite a data de agendamento do colaborador: ");
-            String dataAgendamento = sc.nextLine();
+                System.out.print("Digite o telefone do colaborador: ");
+                String telefone = sc.nextLine();
 
-            System.out.print("Digite o período (<M>anhã, <T>arde ou <N>oite): ");
-            String periodo = sc.nextLine();
+                System.out.print("Digite a data de nascimento do colaborador: ");
+                String dataNascimento = sc.nextLine();
 
-            Pessoa colaborador = new Pessoa(nome, cpf, telefone, dataNascimento, dataAgendamento, periodo);
+                System.out.print("Digite a data de agendamento do colaborador: ");
+                String dataAgendamento = sc.nextLine();
 
-            colaboradores.add(colaborador);
+                System.out.print("Digite o período (<M>anhã, <T>arde ou <N>oite): ");
+                String periodo = sc.nextLine();
 
-            sc.close();
+                Pessoa colaborador = new Pessoa(nome, cpf, telefone, dataNascimento, dataAgendamento, periodo);
+
+                colaboradores.add(colaborador);
+
+                sc.close();
+            }
+            System.out.println("Lista de Colaboradores \n" + colaboradores);
+        } catch (InputMismatchException ex){
+            System.out.println("Rote o programa novamente e passe um numero não String " + ex);
         }
-        System.out.println("Lista de Colaboradores \n" + colaboradores);
     }
 }
